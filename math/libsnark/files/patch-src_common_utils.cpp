c56c8ce83af9fc9389a69204c7b1c833b80267f5
Eric McCorkle <eric@metricspace.net>
2017-01-06 19:01:31 -0500

    Add libsnark port (zcash dependency)

---
+++ math/libsnark/files/patch-src_common_utils.cpp
@@ -0,0 +1,10 @@
+--- src/common/utils.cpp.orig	2016-10-16 17:24:21 UTC
++++ src/common/utils.cpp
+@@ -7,6 +7,7 @@
+  * @copyright  MIT license (see LICENSE file)
+  *****************************************************************************/
+ 
++#include <stdio.h>
+ #include <algorithm>
+ #include <cassert>
+ #include <cstdint>
---
+++ math/libsnark/files/patch-src_gadgetlib1_constraint__profiling.cpp
@@ -0,0 +1,10 @@
+--- src/gadgetlib1/constraint_profiling.cpp.orig	2016-10-16 17:24:21 UTC
++++ src/gadgetlib1/constraint_profiling.cpp
+@@ -11,6 +11,7 @@
+  * @copyright  MIT license (see LICENSE file)
+  *****************************************************************************/
+ 
++#include <stdio.h>
+ #include "gadgetlib1/constraint_profiling.hpp"
+ #include "common/profiling.hpp"
+ 
